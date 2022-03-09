e7eecd782abf2d69adb05037678c4dd3588301ed
Xu Xing <xing.xu@intel.com>
2022-03-09 23:13:08 +0800

    Update runurl.js

--- runurl.js
+++ runurl.js
@@ -23,46 +23,47 @@ async function waitForCondition(condition) {
 }
 
 
-const browserPath = `${process.env.LOCALAPPDATA}/Google/Chrome SxS/Application/chrome.exe`;
+const browserPath =
+    `${process.env.LOCALAPPDATA}/Google/Chrome SxS/Application/chrome.exe`;
 const userDataDir = `${process.env.LOCALAPPDATA}/Google/Chrome SxS/User Data`;
 
 function log(info, logFile) {
-  console.log(info);
+  // console.log(info);
   const fs = require('fs');
   fs.appendFileSync(logFile, String(info) + '\n');
 }
 
 async function startContext(exitCondition, tracingFile, logFile) {
-    let browserArgs = `--disable-dawn-features=disallow_unsafe_apis  --enable-dawn-features=record_detailed_timing_in_trace_events,disable_timestamp_query_conversion --enable-unsafe-webgpu --enable-tracing=disabled-by-default-gpu.dawn  --trace-startup-file=${tracingFile} --trace-startup-format=json`;
-    console.log(browserArgs);
-    let context = await chromium.launchPersistentContext(userDataDir, {
-      headless: false,
-      executablePath: browserPath,
-      viewport: null,
-      ignoreHTTPSErrors: true,
-      args: browserArgs.split(' '),
-    });
-    let page = await context.newPage();
-    page.on('console', async msg => {
-      for (let i = 0; i < msg.args().length; ++i) {
-        log(`[console] ${i}: ${await msg.args()[i].jsonValue()}`, logFile);
-      }
+  let browserArgs =
+      `--disable-dawn-features=disallow_unsafe_apis  --enable-dawn-features=record_detailed_timing_in_trace_events,disable_timestamp_query_conversion --enable-unsafe-webgpu --enable-tracing=disabled-by-default-gpu.dawn  --trace-startup-file=${
+          tracingFile} --trace-startup-format=json`;
+  console.log(browserArgs);
+  let context = await chromium.launchPersistentContext(userDataDir, {
+    headless: false,
+    executablePath: browserPath,
+    viewport: null,
+    ignoreHTTPSErrors: true,
+    args: browserArgs.split(' '),
+  });
+  let page = await context.newPage();
+  page.on('console', async msg => {
+    for (let i = 0; i < msg.args().length; ++i) {
+      log(`[console] ${i}: ${await msg.args()[i].jsonValue()}`, logFile);
+    }
 
-      let msgStr = ('' + msg.args()[0]).replace('JSHandle@', '');
-      if (msgStr.includes('gpudataend')) {
-          exitCondition.logEnd = true;
-      } else {
-          // Unsupported.
-      }
-    });
-    page.on("pageerror", (err) => {
-      console.log(err.message)
-    });
-    return [context, page];
+    let msgStr = ('' + msg.args()[0]).replace('JSHandle@', '');
+    if (msgStr.includes('gpudataend')) {
+      exitCondition.logEnd = true;
+    } else {
+      // Unsupported.
+    }
+  });
+  page.on('pageerror', (err) => {console.log(err.message)});
+  return [context, page];
 }
 
 async function closeContext(context) {
-    await context.close();
+  await context.close();
 }
 
 async function runURL(url, tracingFile, logFile) {
